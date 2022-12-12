3338b0a28298957983bc7c7fcd013a0ffd292644
Mathieu GESLIN <94912547+Mathges@users.noreply.github.com>
2022-12-12 11:33:24 +0100

    wip: main background image

--- app/App.js
+++ app/App.js
@@ -3,17 +3,30 @@ import Home from './src/screens/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Header from './src/components/Header';
+import styled from 'styled-components';
+import {ImageBackground} from 'react-native';
+import DefaultTheme from '@react-navigation/native';
 
 const Stack = createNativeStackNavigator();
 
+const navTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+};
+
 const App = () => {
   return (
-    <NavigationContainer>
-      <Header />
-      <Stack.Navigator screenOptions={{headerShown: false}}>
-        <Stack.Screen name="Home" component={Home} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <ImageBackground source={require('./src/assets/background.jpg')}>
+      <NavigationContainer theme={navTheme}>
+        <Header />
+        <Stack.Navigator screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ImageBackground>
   );
 };
 
