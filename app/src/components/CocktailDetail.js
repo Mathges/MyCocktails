82e6901dce04d4b591fdee2ac4b36ec087bb03ae
Mathieu GESLIN <94912547+Mathges@users.noreply.github.com>
2022-12-17 12:24:10 +0100

    wip: CocktailDetail component now render a formatted ingredient list and the recipe

--- app/src/components/CocktailDetail.js
+++ app/src/components/CocktailDetail.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Image} from 'react-native';
 import styled from 'styled-components';
 
 const CocktailDetail = cocktail => {
+  const [ingredientList, setIngredientList] = useState([]);
+
+  useEffect(() => {
+    const formattedIngredients = [];
+
+    // this one is a bit tricky and dirty i'll need to clarify that
+    // for now, i'll just add comments
+    // the purpose is to format ingredients with measures
+    const formatIngredients = () => {
+      // I iniate 2 arrays, 1 for ingredients, 1 for measures
+      const ingredients = [];
+      const measures = [];
+
+      // I iterate over the objet received, and catch all ingredients/measures to put it in their respective array
+      for (const [key, value] of Object.entries(cocktail)) {
+        if (key.includes('Ingredient') && value !== null) {
+          ingredients.push(value);
+        } else if (key.includes('Measure') && value !== null) {
+          measures.push(value);
+        }
+      }
+
+      // Api sometimes will have ingredients without measure
+      // This is why the while condition is on ingredient array length
+      let count = 0;
+      while (count < ingredients.length) {
+        // if the measure is truthy, I add it to another array
+        if (!!measures[count] !== false) {
+          formattedIngredients[count] = `${measures[count]} of ${ingredients[count]}`;
+        } else {
+          // if not, I add only the ingredient
+          formattedIngredients[count] = `${ingredients[count]}`;
+        }
+        count++;
+      }
+    };
+
+    formatIngredients();
+    // there I format the content of the array to transform it as component and put it to the state
+    // it will make it easier to render
+    setIngredientList(
+      formattedIngredients.map(ingredient => <Text>${ingredient}</Text>),
+    );
+  }, [cocktail]);
+
   return (
     <CardContainer>
       <DetailCard>
-        <StyledText>{cocktail.strDrink}</StyledText>
+        <MainTitle>{cocktail.strDrink}</MainTitle>
         <Image
           source={{uri: cocktail.strDrinkThumb}}
           style={{width: 250, height: 250}}
         />
+        <View>{ingredientList}</View>
+        <Text>{cocktail.strInstructions}</Text>
       </DetailCard>
     </CardContainer>
   );
@@ -28,7 +75,7 @@ const DetailCard = styled.View`
   background-color: rgba(255, 255, 255, 0.3);
   width: 75%;
 `;
-const StyledText = styled.Text`
+const MainTitle = styled.Text`
   color: #fff;
   font-size: 40px;
 `;
